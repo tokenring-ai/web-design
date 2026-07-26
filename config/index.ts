ef27c59e3135e662069d32a2e0c6aaec565f8c2c
Mark Dierolf <mdierolf@financialcontent.com>
2026-07-25 18:03:22 -0700

    Fix minor formatting issue in type annotation for `web-design` plugin config

--- config/index.ts
+++ config/index.ts
@@ -2,4 +2,4 @@ import deepClone from "@tokenring-ai/utility/object/deepClone";
 import webDesignAgentConfig from "./agents/web-design.yaml" with { type: "yaml" };
 import uiExpertToolConfig from "./tools/ui-expert.yaml" with { type: "yaml" };
 
-export default deepClone(webDesignAgentConfig, uiExpertToolConfig) as Record<string,unknown>;
+export default deepClone(webDesignAgentConfig, uiExpertToolConfig) as Record<string, unknown>;
